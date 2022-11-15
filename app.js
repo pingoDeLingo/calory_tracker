97a7232ac0e5608d5a55db38e06955d2236e7617
Robin <robin.noormets@voco.ee>
2022-11-15 15:21:45 +0200

    Populated data srtucture items to list

--- app.js
+++ app.js
@@ -1,37 +1,53 @@
 const ItemCtrl = (function(){
-    const Item = function(id, name, calories){
+    const Item = function(id,name,calories){
         this.id = id
         this.name = name
         this.calories = calories
     }
-
     const data = {
         items: [
-            {id: 0, name: 'Steak Dinner', calories: 1200},
-            {id: 1, name: 'Cookie', calories: 400},
-            {id: 2, name: 'Eggs', calories: 300}
+            {id:0, name:"Steak", calories: 1200},
+            {id:1, name:"Cookie", calories: 400},
+            {id:2, name:"Eggs", calories: 300}
         ],
         total: 0
     }
-    return {
+
+    return{
+        getItems: function(){
+            return data.items
+        },
         logData: function(){
             return data
         }
     }
 })();
-
-
-const UICtrl = (function () {
-    console.log('UICtrl')
-})
-
-
-const App = (function (ItemCtrl, UICtrl){
+const UICtrl = (function(){
     return {
-        init: function (){
-            console.log("Initializing app")
+        populateItemList: function(items){
+            let html= "";
+
+            items.forEach(
+                function(item) {
+                    html += `<li class="collection-item" id="item-${item.id}">
+                    <strong>${item.name}:</strong> <em>${item.calories} Calories</em>
+                    <a href="#" class="secondary-content">
+                        <i class="edit-item fa fa-pencil"></i>
+                    </a>
+                    </li>`;
+                });
+            document.querySelector("#item-list").innerHTML = html;
         }
     }
-})(ItemCtrl, UICtrl)
+})();
+const App = (function(ItemCtrl,UICtrl){
+    return{
+        init:function(){
+            console.log("Initializing App")
+            const items = ItemCtrl.getItems()
+            UICtrl.populateItemList(items)
+        }
+    }
+})(ItemCtrl, UICtrl);
 
-App.init
+App.init()
